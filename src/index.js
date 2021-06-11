78ff8b3231528865e7ed9393e3fb260fec270934
AppleJaxxZ <smackanalex@gmail.com>
2021-06-11 14:20:11 -0400

    reapplied strictmode

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 
   document.getElementById("root")
 );
