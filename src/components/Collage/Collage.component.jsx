844ba289415dfd6b73f2515039af46da7b53b99c
AppleJaxxZ <smackanalex@gmail.com>
2021-11-16 15:39:48 -0500

    changed background image, removed movie knight, added peackcock styled colored dots

--- src/components/Collage/Collage.component.jsx
+++ src/components/Collage/Collage.component.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import MovieCollage from "../../media/moviecollage.jpg";
+import MovieCollage from "../../media/movieknight-background.webp";
 import "./Collage.styles.scss";
 
 const Collage = () => {
   return (
     <div className="collage">
       <img className="collage-img" src={MovieCollage} alt="movies collage" />
+      <span className="text-overlay">
+        A Peacock semi-clone..
+        <br /> Your favorite movies...
+        <br /> All here on... <u>Movie Knight</u>
+        <button className="homepage-signup">Sign Up</button>
+      </span>
     </div>
   );
 };
--- src/components/Header/Header.components.jsx
+++ src/components/Header/Header.components.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import "../Header/Header.styles.css";
+import "../Header/Header.styles.scss";
 import CartoonKnight from "../../media/cartoonKnight.png";
 
 const Header = () => (
   <div className="header-container">
     <h1 className="header">Movie Knight</h1>
-    <h4 id="caption">Your favorite movies on Movie Knight.</h4>
-    <img id="knight" src={CartoonKnight} alt="knight icon" />
+    <div className="header__dots-flexed">
+      <div className="dot" id="one"></div>
+      <div className="dot" id="two"></div>
+      <div className="dot" id="three"></div>
+      <div className="dot" id="four"></div>
+      <div className="dot" id="five"></div>
+      <div className="dot" id="six"></div>
+    </div>
   </div>
 );
 
